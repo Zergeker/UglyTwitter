3846d7b04e9fffd9e8d1400eaf0b3b0269c3a100
nikita7777o <45508813+nikita7777o@users.noreply.github.com>
2020-04-02 01:38:44 +0500

    tables aded

---
+++ src/main/java/com/project/uglyTwitter/Post.java
@@ -0,0 +1,43 @@
+package com.project.uglyTwitter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long id;
+    private String title;
+    private String text_content;
+    private Long author_id;
+
+    protected Post() {}
+
+    public Post(String firstName, String lastName) {
+        this.title = firstName;
+        this.text_content = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, title='%s', text_content='%s']",
+                id, title, text_content);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText_content() {
+        return text_content;
+    }
+}
